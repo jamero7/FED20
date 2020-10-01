30a98691c92cd1ad533c6a4f340131eb61b5208c
Jamero <43680467+jamero7@users.noreply.github.com>
2020-10-01 16:39:44 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,21 @@ const volgend = document.querySelector("#volgend");
 const meldingen = document.querySelector("#meldingen");
 const bewaard = document.querySelector("#bewaard");
 
+const startpaginaicoon = document.querySelector("#startpaginaicoon");
+const volgendicoon = document.querySelector("#volgendicoon");
+const meldingenicoon = document.querySelector("#meldingenicoon");
+const bewaardicoon = document.querySelector("#bewaardicoon");
+
 startpagina.addEventListener("click", function(){
     startpagina.classList.add("actief");
     volgend.classList.remove("actief");
     meldingen.classList.remove("actief");
     bewaard.classList.remove("actief"); 
+    
+    startpaginaicoon.classList.add("actief");
+    volgendicoon.classList.remove("actief");
+    meldingenicoon.classList.remove("actief");
+    bewaardicoon.classList.remove("actief"); 
 });
 
 volgend.addEventListener("click", function(){
@@ -16,6 +26,11 @@ volgend.addEventListener("click", function(){
     volgend.classList.add("actief");
     meldingen.classList.remove("actief");
     bewaard.classList.remove("actief"); 
+    
+    startpaginaicoon.classList.remove("actief");
+    volgendicoon.classList.add("actief");
+    meldingenicoon.classList.remove("actief");
+    bewaardicoon.classList.remove("actief"); 
 });
 
 meldingen.addEventListener("click", function(){
@@ -23,10 +38,21 @@ meldingen.addEventListener("click", function(){
     volgend.classList.remove("actief");
     meldingen.classList.add("actief");
     bewaard.classList.remove("actief"); 
+    
+    startpaginaicoon.classList.remove("actief");
+    volgendicoon.classList.remove("actief");
+    meldingenicoon.classList.add("actief");
+    bewaardicoon.classList.remove("actief"); 
 });
+
 bewaard.addEventListener("click", function(){
     startpagina.classList.remove("actief");
     volgend.classList.remove("actief");
     meldingen.classList.remove("actief");
     bewaard.classList.add("actief"); 
+    
+    startpaginaicoon.classList.remove("actief");
+    volgendicoon.classList.remove("actief");
+    meldingenicoon.classList.remove("actief");
+    bewaardicoon.classList.add("actief"); 
 }); 
