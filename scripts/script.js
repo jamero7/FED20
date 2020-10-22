5383222c35a2e22c2b5c36eaf0ade340b95e0712
Jamero <43680467+jamero7@users.noreply.github.com>
2020-10-22 10:26:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,52 @@ const startpagina = document.querySelector("#startpagina");
 const volgend = document.querySelector("#volgend");
 const meldingen = document.querySelector("#meldingen");
 const bewaard = document.querySelector("#bewaard");
+const detailOpties = document.querySelector("#detailOpties");
+const optiesMenu2 = document.querySelector("#optiesMenu2");
+const sluiten = document.querySelector(".optiesMenu button");
+const sluiten2 = document.querySelector("#optiesMenu2 button");
 
-var actief = document.getElementsByClassName("actief");
+let opties = document.querySelector(".optiesKnop");
+let optiesMenu = document.querySelector(".optiesMenu");
 
 startpagina.addEventListener("click", function(){
+    let actief = document.querySelector(".actief");
     actief.classList.remove("actief");
+    startpagina.classList.add("actief");
 });
 
 volgend.addEventListener("click", function(){
+    let actief = document.querySelector(".actief");
     actief.classList.remove("actief"); 
+    volgend.classList.add("actief");
 });
 
 meldingen.addEventListener("click", function(){
+    let actief = document.querySelector(".actief");
     actief.classList.remove("actief");
+    meldingen.classList.add("actief");
 });
 
 bewaard.addEventListener("click", function(){
+    let actief = document.querySelector(".actief");
     actief.classList.remove("actief");
+    bewaard.classList.add("actief");
 }); 
+
+opties.addEventListener("click", function(){
+    optiesMenu.style.display = "block";
+});
+
+sluiten.addEventListener("click", function(){
+    optiesMenu.style.display = "none";
+    console.log("wagwan");
+});
+
+detailOpties.addEventListener("click", function(){
+    optiesMenu2.style.display = "block";
+});
+
+sluiten2.addEventListener("click", function(){
+    optiesMenu2.style.display = "none";
+    console.log("wagwan");
+});
